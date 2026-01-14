d1b5df68ed506545f23535d09122abc82982ae0f
Tomasz Pilarczyk <tomasz.pilarczyk@icloud.com>
2026-01-14 22:30:50 +0100

    Initial commit

---
+++ src/main/java/com/gosu/firsttake/FirsttakeApplication.java
@@ -0,0 +1,13 @@
+package com.gosu.firsttake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsttakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsttakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gosu/firsttake/FirsttakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosu.firsttake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsttakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
